9bd16f4919f7c195adb419627d623433e484581d
Mubashir <hmubashir08@gmail.com>
2020-06-19 19:12:24 +0530

    final changes

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -12,27 +12,40 @@ import * as sample2 from '../assets/sample2.json';
 })
 export class DashboardComponent implements OnInit {
 
+  public week1 = true;
   public table:any = (sample1 as any).default;
-  public days:any = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday'];
+  public days:any = ['Sunday','Monday','tuesday','Wednesday','Thursday','Friday','Saturday'];
   constructor() { 
   }
 
   sample1() {
     this.table = (sample1 as any).default;
+    this.week1 = true;
   }
   sample2() {
     this.table = (sample2 as any).default;
+    this.week1 = false;
   }
 
-  timing( day, data) {
-    if ( day = data.split(' ')[0]) {
-      console.log(data);
+  timing( day, key, data) {
+    let dataArray = data[key];
+    let time = dataArray.find( res => {
+      let split = res.split(' ')
+      if ( split[0] === day) {
+        let value = split[2];
+        return value;
+      } else {
+        return "";
+      }
+    })
+    if( time != undefined ) {
+      let finalTime = time.split(" ");
+      return(finalTime[2]+finalTime[3]+finalTime[4]+finalTime[5]+finalTime[6])
     }
-    // return time;
   }
 
   ngOnInit(): void {
-    console.log(this.table);
+    // console.log(this.table.Bartender);
     
   }
   
